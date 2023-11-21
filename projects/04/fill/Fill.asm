1f42898a2467a89b1c0acf50ab6427a24afa0819
Kaustubh Tripathi <kaustubhtripathi@Kaustubhs-MacBook-Air.local>
2023-11-21 20:40:31 +0530

    Project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,110 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 //// Replace this comment with your code.
+
+// take input from the register mapped to keyboard input 
+// then if value is >0 go to blacken function 
+// that runs a loop for memory map of the screen and makes them black 
+// then return back to original function where we take input 
+// similarly the mirror case
+
+(TAKE_INPUT)
+
+    @24575
+    D=A
+    @1
+    M=D
+
+    @24576 // kb input
+    D=M
+
+    @BLACKEN
+    D;JGT
+
+    @24576 
+    D=M
+
+    @WHITEN
+    D;JEQ
+
+(BLACKEN)
+
+    @16384
+    D=A
+    @2
+    M=D
+
+    @1
+    D=M-D
+
+    @LOOP_B
+    D;JGE
+
+    @RETURN
+    D;JLT
+
+(LOOP_B)
+    // 2 has current address that need to be adressed
+    @2
+    A=M
+    M=-1  // apparently the code is -1 not 1 
+
+    @2
+    M=M+1
+    D=M
+
+    @24575
+    D=A-D
+
+    @1
+    M=D
+
+    @LOOP_B
+    D;JGE
+
+    @RETURN
+    D;JLT
+
+(RETURN)
+
+    @TAKE_INPUT
+    D;JLT
+
+(WHITEN)
+
+    @16384
+    D=A
+
+    @2
+    M=D
+
+    @1
+    D=M-D
+
+    @LOOP_W
+    D;JGE
+
+    @RETURN
+    D;JLT
+
+(LOOP_W)
+
+    @2
+    A=M
+    M=0
+
+    @2
+    M=M+1
+    D=M
+
+    @24575
+    D=A-D
+
+    @1
+    M=D
+
+    @LOOP_W
+    D;JGE
+
+    @RETURN
+    D;JLT
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -8,3 +8,30 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 //// Replace this comment with your code.
+    @2   // loading R2 (Memory[2]) in A register
+    M=0  //  M is basically Memory[A]
+
+    @0
+    D=M
+    @LOOP // Loading LOOP in A
+    D;JGT
+
+    @0
+    D=M
+    @END
+    D;JLE
+
+(LOOP)
+
+    @0
+    M=M-1  //   R0-=1;
+    @1
+    D=M;   // storing R1
+    @2
+    M=M+D  // R2 = R2 + R1
+    @0
+    D=M
+    @LOOP
+    D;JGT
+
+(END)
